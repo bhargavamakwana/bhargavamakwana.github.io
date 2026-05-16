384d8a38611122e4ee313be05f54b309387bc677
bhargavamakwana <bhargavamakwana@gmail.com>
2026-05-16 12:04:35 +0530

    Fix Build Errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import About from './pages/About';
-import Blog from './pages/Blog';
+// import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 
 function App() {
